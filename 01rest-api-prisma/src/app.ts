9136f24e7c033f19be8935669c0583860ce6eff3
Reo Nishida <leo.nishida1030@gmail.com>
2025-11-12 12:59:33 +0900

    feat: implement CRUD api for posts

--- 01rest-api-prisma/src/app.ts
+++ 01rest-api-prisma/src/app.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import postRoutes from "./routes/postRoutes";
 
 const app = express();
 app.use(express.json());
+app.use("/api/posts", postRoutes);
 
 app.get('/',(req,res) => {
   res.send("Hello Prisma API!!")
