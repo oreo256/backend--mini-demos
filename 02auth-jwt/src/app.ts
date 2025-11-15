08bbb39738fbe80c2c34ab9ede1502b6acdf0fd6
Reo Nishida <leo.nishida1030@gmail.com>
2025-11-15 14:06:41 +0900

    feat: add auth route and controller skeletons

--- 02auth-jwt/src/app.ts
+++ 02auth-jwt/src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { Request,Response } from "express";
+import authRoutes from "./routes/authRoutes"
 
 const app = express();
 app.use(express.json());
+app.use("/api/auth",authRoutes);
 
 app.get("/",(req:Request, res:Response) => {
   res.send("Hello");
---
+++ 02auth-jwt/src/controllers/authController.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+
+export const register = (req: Request, res: Response) => {
+  res.status(501).json({ error: "Not implemented" });
+}
+
+export const login = (req: Request, res: Response) => {
+  res.status(501).json({ error: "Not implemented" });
+}
