0208d68f69257da4ed372f74c2ce5372343977cb
Reo Nishida <leo.nishida1030@gmail.com>
2025-11-16 13:22:02 +0900

    feat: implement register endpoint

--- 02auth-jwt/src/controllers/authController.ts
+++ 02auth-jwt/src/controllers/authController.ts
@@ -1,7 +1,25 @@
 import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt"
 
-export const register = (req: Request, res: Response) => {
-  res.status(501).json({ error: "Not implemented" });
+const prisma = new PrismaClient();
+
+export const register = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  try {
+    const hashed = await bcrypt.hash(password, 10);
+    const user = await prisma.user.create({
+      data: { email, password: hashed },
+    })
+
+    res.status(201).json({
+      message: "User created",
+      user: { id: user.id, email: user.email }
+    })
+  } catch (error) {
+    res.status(400).json({ error: "Email already exists" })
+  }
 }
 
 export const login = (req: Request, res: Response) => {
