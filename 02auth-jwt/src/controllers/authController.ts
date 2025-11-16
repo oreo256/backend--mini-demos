b6005fa9b804f3056a88e0206a309b8f1674ebb4
Reo Nishida <leo.nishida1030@gmail.com>
2025-11-16 16:03:54 +0900

    feat: implement login endpoint

--- 02auth-jwt/src/controllers/authController.ts
+++ 02auth-jwt/src/controllers/authController.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken";
 
 const prisma = new PrismaClient();
+const JWT_SECRET = "secret-key";
 
 export const register = async (req: Request, res: Response) => {
   const { email, password } = req.body;
@@ -22,6 +24,18 @@ export const register = async (req: Request, res: Response) => {
   }
 }
 
-export const login = (req: Request, res: Response) => {
-  res.status(501).json({ error: "Not implemented" });
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  const user = await prisma.user.findUnique({ where: { email } });
+  if (!user) return res.status(401).json({ error: "Invalid credentials" });
+
+  const isValid = await bcrypt.compare(password, user.password);
+  if (!isValid) return res.status(401).json({ error: "Invalid credentails" });
+
+  const token = jwt.sign({ userId: user.id }, JWT_SECRET, {
+    expiresIn: "1h"
+  });
+
+  res.json({ message: "Login successful", token });
 }
