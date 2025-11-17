50d5a31aa62e88d1ebca8c788d06f8cf4f8293ef
Reo Nishida <leo.nishida1030@gmail.com>
2025-11-17 21:33:37 +0900

    feat: implement protected profile route

--- 02auth-jwt/src/app.ts
+++ 02auth-jwt/src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { Request,Response } from "express";
 import authRoutes from "./routes/authRoutes"
+import protectedRoutes from "./routes/protectedRoutes";
 
 const app = express();
 app.use(express.json());
 app.use("/api/auth",authRoutes);
+app.use("/api", protectedRoutes);
 
 app.get("/",(req:Request, res:Response) => {
   res.send("Hello");
--- 02auth-jwt/src/routes/protectedRoutes.ts
+++ 02auth-jwt/src/routes/protectedRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { Request,Response } from "express";
+import { Request, Response } from "express";
+import { requireAuth } from "../middlewares/auth";
 
 const router = express.Router();
 
-router.get("/profile", (req:Request, res:Response) =>{
-  res.status(501).json({error: "profile not implemented"});
+router.get("/profile", requireAuth, (req: Request, res: Response) => {
+  res.json({ message: "This is a protected route", user: (req as any).user });
 })
 
 export default router;
